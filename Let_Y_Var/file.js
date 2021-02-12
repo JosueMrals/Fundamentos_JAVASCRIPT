e092b08e7c4bc6ec3718633a01dcc18356cc5244
@Josue <62188147+jshkey@users.noreply.github.com>
2021-02-12 10:19:29 -0600

    Let y Var

---
+++ Let_Y_Var/file.js
@@ -0,0 +1,19 @@
+//let id_carro = 42; 
+//console.log(id_carro); //error
+
+//console.log(id_carro);//undefined(no error)
+//var id_carro = 42; 
+
+/*
+if(true)
+{ 
+	var id_carro = 9;
+} 
+console.log(id_carro);//9
+*/
+
+if(true)
+{ 
+	let id_carro = 9;
+} 
+console.log(id_carro); //error no defined
