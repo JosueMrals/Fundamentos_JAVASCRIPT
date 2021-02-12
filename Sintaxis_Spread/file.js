bcd6b337973ef3fd1b5679a3451bca69e8bed257
@Josue <62188147+jshkey@users.noreply.github.com>
2021-02-12 11:16:08 -0600

    Sintaxis Spread

---
+++ Sintaxis_Spread/file.js
@@ -0,0 +1,13 @@
+/*function iniciarCarros(car1,car2,car3){ 
+    console.log(car1, car2, car3);
+ }
+ let idCarros = [100,200,300];
+iniciarCarros(...idCarros);
+//100 200 300*/
+
+function iniciarCarros(car1,car2,car3){ 
+    console.log(car1, car2, car3);
+ }
+ let codigoCarros = 'abc'
+iniciarCarros(...codigoCarros);
+//a  b  c
