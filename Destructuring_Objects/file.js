5b9089791aa7427c3b9a23d7b645f1e4bc1116eb
@Josue <62188147+jshkey@users.noreply.github.com>
2021-02-12 11:07:52 -0600

    Destructuring Objects 2

--- Destructuring_Objects/file.js
+++ Destructuring_Objects/file.js
@@ -1,10 +1,16 @@
-let carro = {id:20,estilo: 'convertible'}; 
+/*let carro = {id:20,estilo: 'convertible'}; 
 let {id,estilo} = carro;
 console.log(id,estilo);
 //500 convertible (20 convertible)
 
-/*
+
 let carro = {id:20,estilo: 'convertible'};
 let id,estilo;
 {id,estilo} = carro; //error (Inicializador desaparecido, se esperaba una declaracion o una instruccion)
 console.log(id,estilo); */
+
+let carro = {id:20,estilo: 'convertible'};
+let id,estilo;
+({id,estilo} = carro); 
+console.log(id,estilo); 
+// 20 convertible
