dbc8d0e7a624b1301b5725d7f81cac7bd1864f8f
@Josue <62188147+jshkey@users.noreply.github.com>
2021-02-15 09:19:00 -0600

    TypeOf

---
+++ TypeOf/file.js
@@ -0,0 +1,9 @@
+/*typeof(1); 	// number
+typeof(true); // boolean
+typeof('Hola'); // string
+typeof(function(){}); 	// function
+typeof({}); 	// object
+typeof(null); 	// object
+typeof(undefined); 	// undefined0*/
+typeof(NaN); 	// number
+
