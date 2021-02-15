62e91998ea27d70d6aebcac3afe5082a3af16d0e
@Josue <62188147+jshkey@users.noreply.github.com>
2021-02-15 13:20:30 -0600

    Ambito de Bloque

---
+++ Ambito_De_Bloque/file.js
@@ -0,0 +1,11 @@
+/*if (5 == 5){ 
+    let mensaje = 'Igual...';
+ } 
+console.log(mensaje); // error */
+
+let mensaje = 'Fuera...';
+if (5 == 5){ 
+   let mensaje = 'Igual...';
+   console.log(mensaje); // Igual
+}
+   console.log(mensaje); // Fuera
