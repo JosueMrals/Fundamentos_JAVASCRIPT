be1e22f2d95f18ee4273aaeae1830b06352511f7
@Josue <62188147+jshkey@users.noreply.github.com>
2021-02-15 09:32:19 -0600

    Ciclos Controlados

---
+++ Ciclos_Controlados/file.js
@@ -0,0 +1,16 @@
+/*let i = 0;
+for (;i<12;i++){
+   if(i===8){
+     break;
+   }
+}
+console.log(i); //8 */
+
+for (let i=0;i<4;i++){
+    if(i===2){
+      continue;
+    }
+    console.log(i);
+ }
+ // 0 1 3
+
