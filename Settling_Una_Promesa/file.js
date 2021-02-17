e4f22d55100373dd8170390b982a3f0453d8c292
@Josue <62188147+jshkey@users.noreply.github.com>
2021-02-17 12:10:15 -0600

    Settling Una Promesa

---
+++ Settling_Una_Promesa/file.js
@@ -0,0 +1,10 @@
+let promesa = new Promise(
+    function(resolve,reject){
+       setTimeOut(resolve,100,'Otro Valor');
+    }
+ );
+ promesa.then(
+   value => console.log("resuelto:" + value);
+   error => console.log("rechazado:" + error);
+ );
+ 
